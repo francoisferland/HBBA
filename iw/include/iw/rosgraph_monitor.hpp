francoisferland/HBBA

commit 0a5a6967a782d2387602dde7cf4cbfe913098c45

Refs #376. Importing and adapting rosgraph_gl monitor-related structures for perception-related HBBA events.
git-svn-id: svn+ssh://marvin.3it.usherbrooke.ca/svn/introlab/johnny0/svn/trunk/ros-pkgs/hbba@3507 23a761d4-b60a-4777-9503-f06a07968eac

iw/include/iw/rosgraph_monitor.hpp
@@ -1,0 +1,98 @@
+///
+/// \file rosgraph_client.hpp \brief Copied from rosgraph_gl and adapted to iw.
+/// 
+
+#ifndef ROSGRAPH_MONITOR_HPP
+#define ROSGRAPH_MONITOR_HPP
+
+#include <iw/RosgraphEvents.h>
+#include <topic_tools/shape_shifter.h>
+#include <ros/ros.h>
+#include <boost/shared_ptr.hpp>
+
+namespace iw
+{
+    namespace impl
+    {
+        /// \brief Topic subscription handler for RosgraphMonitor.
+        class TopicHandler
+        {
+        private:
+            std::string     topic_name_;
+            ros::Subscriber sub_;
+            int             counter_;
+
+        public:
+            /// \brief Constructor.
+            ///
+            /// Automatically subscribes to the given topic name through the
+            /// provided node handle. 
+            /// The internal counter is set to 0 and incremented each time a
+            /// message is received.
+            ///
+            /// \param n          Node handle to use for subscription.
+            /// \param topic_name The subscribed topic's name.
+            ///
+            TopicHandler(ros::NodeHandle& n, const std::string& topic_name);
+
+            /// \brief Return this handler's topic name.
+            ///
+            /// This is the value as configured in the constructor, not as 
+            /// resolved by the node handle.
+            const std::string& topicName() const;
+
+            /// \brief Return the current counter value.
+            int counter() const;
+
+            /// \brief Reset the counter value to 0.
+            void resetCounter();
+
+        private:
+            void msgCB(const topic_tools::ShapeShifter::ConstPtr&);
+        };
+    } 
+
+    /// \brief A topic-based rosgraph monitor.
+    ///
+    /// This node monitors messages on a (pre-configured) set of topics and
+    /// an activity log.
+    /// On a regular basis, the activity log for the monitoring period is sent
+    /// on an output topic and flushed for the next period.
+    ///
+    /// Parameters:
+    /// 
+    ///  - topics: An array of strings, each one a topic to monitor.
+    ///            Default: empty.
+    ///  - period: The time period between log sendings.
+    ///            Default: 0.1 s.
+    /// 
+    /// Topics:
+    ///  - rosgraph_activity: A vector of RosgraphEvent structs.
+    ///
+    class RosgraphMonitor
+    {
+    private:
+        typedef impl::TopicHandler              TopicHandler;
+        typedef boost::shared_ptr<TopicHandler> TopicHandlerPtr;
+        typedef std::vector<TopicHandlerPtr>    TopicHandlers;
+
+        ros::Publisher pub_events_;
+        ros::Timer     timer_;
+        TopicHandlers  topic_handlers_;
+
+    public:
+        /// \brief Constructor.
+        ///
+        /// \param n  Node handle for topics.
+        /// \param np Node handle for parameters.
+        RosgraphMonitor(ros::NodeHandle& n, ros::NodeHandle& np);
+
+
+    private:
+        void timerCB(const ros::TimerEvent&);
+
+    };
+}
+
+#endif
+

iw/src/rosgraph_monitor.cpp
@@ -1,0 +1,88 @@
+///
+/// \file rosgraph_monitor.cpp \brief Copied from rosgraph_gl and adapted to iw.
+///
+
+#include <iw/rosgraph_monitor.hpp>
+
+using namespace iw;
+
+impl::TopicHandler::TopicHandler(
+    ros::NodeHandle& n, 
+    const std::string& topic_name):
+        topic_name_(topic_name),
+        counter_(0)
+{
+    sub_ = n.subscribe(topic_name, 10, &TopicHandler::msgCB, this);
+}
+
+void impl::TopicHandler::msgCB(const topic_tools::ShapeShifter::ConstPtr&)
+{
+    ++counter_;
+}
+
+const std::string& impl::TopicHandler::topicName() const
+{
+    return topic_name_;
+}
+
+int impl::TopicHandler::counter() const
+{
+    return counter_;
+}
+
+void impl::TopicHandler::resetCounter()
+{
+    counter_ = 0;
+}
+
+RosgraphMonitor::RosgraphMonitor(ros::NodeHandle& n, ros::NodeHandle& np)
+{
+    if (np.hasParam("topics")) {
+        XmlRpc::XmlRpcValue array;
+        np.getParam("topics", array);
+        if (array.getType() != XmlRpc::XmlRpcValue::TypeArray) {
+            ROS_ERROR("'topics' parameter is not an array!");
+            return;
+        }
+        for (int i = 0; i < array.size(); ++i) {
+            XmlRpc::XmlRpcValue topic_name = array[i];
+            if (topic_name.getType() != XmlRpc::XmlRpcValue::TypeString) {
+                ROS_ERROR("'topics' element is not a string, skipping.");
+                continue;
+            }
+            topic_handlers_.push_back(TopicHandlerPtr(new TopicHandler(
+                n,
+                topic_name)));
+        }
+    } else {
+        ROS_WARN("No topic names provided ('topics' parameter not found).");
+        return;
+    }
+
+    pub_events_ = n.advertise<RosgraphEvents>("rosgraph_events", 10);
+
+    double p;
+    np.param("period", p, 0.10);
+    timer_ = n.createTimer(ros::Duration(p), &RosgraphMonitor::timerCB, this);
+}
+
+void RosgraphMonitor::timerCB(const ros::TimerEvent&)
+{
+    RosgraphEvents events;
+    typedef TopicHandlers::iterator It;
+    for (It i = topic_handlers_.begin(); i != topic_handlers_.end(); ++i) {
+        TopicHandlerPtr th = *i;
+        if (th->counter()) {
+            RosgraphEvent e;
+            e.topic_name = th->topicName();
+            e.activity   = th->counter();
+            events.events.push_back(e);
+            th->resetCounter();
+        }
+    }
+
+    if (events.events.size()) {
+        pub_events_.publish(events);
+    }
+}
+

hbba_msgs/msg/RosgraphEvent.msg
@@ -1,0 +1,3 @@
+string topic_name # Name of the topic
+int64  activity   # Activity counter, usually how many messages were received.
+

hbba_msgs/msg/RosgraphEvents.msg
@@ -1,0 +1,1 @@
+RosgraphEvent[] events

iw/CMakeLists.txt
@@ -28,6 +28,7 @@ rosbuild_add_library(iw
     src/exploitation_matcher.cpp
     src/events_generator.cpp
     src/events_filter.cpp
+    src/rosgraph_monitor.cpp
 )
 rosbuild_add_executable(events_generator src/events_generator_node.cpp)
 target_link_libraries(events_generator iw)

commit 70d735881b4a809afc208e84c57ec97eba5b0a3b

Refs #376. Fixing build issues introduced in r3507.
git-svn-id: svn+ssh://marvin.3it.usherbrooke.ca/svn/introlab/johnny0/svn/trunk/ros-pkgs/hbba@3508 23a761d4-b60a-4777-9503-f06a07968eac

iw/include/iw/rosgraph_monitor.hpp
@@ -5,7 +5,7 @@
 #ifndef ROSGRAPH_MONITOR_HPP
 #define ROSGRAPH_MONITOR_HPP
 
-#include <iw/RosgraphEvents.h>
+#include <hbba_msgs/RosgraphEvents.h>
 #include <topic_tools/shape_shifter.h>
 #include <ros/ros.h>
 #include <boost/shared_ptr.hpp>

iw/manifest.xml
@@ -12,6 +12,7 @@
   <depend package="rospy"/>
   <depend package="hbba_msgs"/>
   <depend package="std_srvs"/>
+  <depend package="topic_tools"/>
 
   <export>
       <cpp cflags="-I${prefix}/msg/cpp -I${prefix}/include" lflags="-L${prefix}/lib -liw"/>

iw/src/rosgraph_monitor.cpp
@@ -59,7 +59,7 @@ RosgraphMonitor::RosgraphMonitor(ros::NodeHandle& n, ros::NodeHandle& np)
         return;
     }
 
-    pub_events_ = n.advertise<RosgraphEvents>("rosgraph_events", 10);
+    pub_events_ = n.advertise<hbba_msgs::RosgraphEvents>("rosgraph_events", 10);
 
     double p;
     np.param("period", p, 0.10);
@@ -68,12 +68,12 @@ RosgraphMonitor::RosgraphMonitor(ros::NodeHandle& n, ros::NodeHandle& np)
 
 void RosgraphMonitor::timerCB(const ros::TimerEvent&)
 {
-    RosgraphEvents events;
+    hbba_msgs::RosgraphEvents events;
     typedef TopicHandlers::iterator It;
     for (It i = topic_handlers_.begin(); i != topic_handlers_.end(); ++i) {
         TopicHandlerPtr th = *i;
         if (th->counter()) {
-            RosgraphEvent e;
+            hbba_msgs::RosgraphEvent e;
             e.topic_name = th->topicName();
             e.activity   = th->counter();
             events.events.push_back(e);
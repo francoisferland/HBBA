a11d6080ef7abe44d482156c00f491c34ccf9d22
ferf2103 <ferf2103@23a761d4-b60a-4777-9503-f06a07968eac>
2013-11-14 21:30:41 +0000

    Refs #376. New generic divider standalone node with rate topic.
    git-svn-id: svn+ssh://marvin.3it.usherbrooke.ca/svn/introlab/johnny0/svn/trunk/ros-pkgs/hbba@3887 23a761d4-b60a-4777-9503-f06a07968eac

--- topic_filters/src/generic_divider.cpp
+++ topic_filters/src/generic_divider.cpp
@@ -1,139 +1,17 @@
-#include <nodelet/nodelet.h>
-#include <pluginlib/class_list_macros.h>
-#include <topic_tools/shape_shifter.h>
-#include <topic_filters/SetDividerRate.h>
-#include <ros/ros.h>
-#include <deque>
+#include "generic_divider.hpp"
+#include <algorithm>
 
-namespace topic_filters
+int main(int argc, char** argv)
 {
-    /// \brief A switch/divider generic topic filter nodelet.
-    ///
-    /// The rate of the divider sets how many messages should be skipped.
-    /// The default is 1.
-    /// A rate of 1 republishes all messages, a rate 2 is half, a rate of 4 is
-    /// a quarter, etc.
-    ///
-    /// A rate of less than 1 stops completely.
-    ///
-    /// In latch mode, the filter sends the last received message as soon
-    /// as it's reactivated.
-    class GenericDivider: public nodelet::Nodelet
-    {
-    public:
-        /// \brief Default constructor. Default rate is 1.
-        GenericDivider(): rate_(0), count_(0), advertised_(false) {}
+    ros::init(argc, argv, "generic_divider");
 
-        virtual void onInit()
-        {
-            const std::vector<std::string>& args = getMyArgv();
-            if (args.size() < 2)
-            {
-                NODELET_ERROR("Filter called with less than 2 args"
-                    " (topic_in, topic_out).");
-                return;
-            }
+    std::vector<std::string> args(argc - 1);
+    std::copy(&argv[1], &argv[argc], args.begin());
 
-            n_ = getNodeHandle();
-            sub_ = n_.subscribe(args[0], 10, &GenericDivider::msgCB, this);
-            output_name_ = args[1];
-            
-            ros::NodeHandle np = getPrivateNodeHandle();
-            srv_rate_ = np.advertiseService("set_divider_rate",
-                &GenericDivider::rateCB, this);
-            int ls;
-            np.param("latch_size", ls, 1);
-            latch_size_ = abs(ls);
+    ros::NodeHandle n, np("~");
+    topic_filters::GenericDividerNode node(n, np, args);
 
-        }
-
-    private:
-        void msgCB(const topic_tools::ShapeShifter::ConstPtr& msg)
-        {
-            if (!advertised_)
-            {
-                // We can't automatically advertise shape shifter topics
-                // before getting a message (and its MD5) first.
-                // Note that this filter couldn't actually change its type
-                // after getting a first message, and that the output topic
-                // will be advertised even if the filter is deactivated.
-                // ROS_INFO("Advertising %s ...", output_name_.c_str());
-                pub_ = msg->advertise(n_, output_name_, 10, latch_size_ > 0);
-                advertised_ = true;
-                // TODO Don't understand why, but at first the publisher think
-                // there is no subscribers, dropping the first message. Latching
-                // seems not working too (will need a retry).
-                // TODO: Figure out if this works in any threading situation.
-                // TODO: Disabled! the script engine get stuck if this happens.
-                //while (pub_.getNumSubscribers() == 0) {
-                //    ros::Duration(0.1).sleep();
-                //}
-            }
-
-            // A rate of 0 will never publish messages, and we need to
-            // avoid divisions by 0.
-            if (rate_ && !(count_ % rate_))
-            {
-                //ROS_INFO("Publishing on %s ...", output_name_.c_str());
-                pub_.publish(msg);
-            }
-
-            //last_msg_ = msg;
-            latch_buffer_.push_back(msg);
-            if (latch_buffer_.size() > latch_size_)
-                latch_buffer_.pop_front();
-
-            count_++;
-        }
-
-        bool rateCB(topic_filters::SetDividerRate::Request& req,
-            topic_filters::SetDividerRate::Response&)
-        {
-            rate_ = req.divider;
-
-            if (rate_ < 0)
-                rate_ = 0;
-
-            ROS_DEBUG("Switching rate to %i", rate_);
-
-            if (rate_ > 0)
-            {
-                if ((latch_size_ > 0) && advertised_)
-                {
-                    while (!latch_buffer_.empty())
-                    {
-                        MsgPtr m = latch_buffer_.front();
-                        pub_.publish(m);
-                        latch_buffer_.pop_front();
-                    }
-                }
-            }
-            else
-            {
-                //ROS_INFO("Turning off %s", output_name_.c_str());
-            }
-
-            return true;
-        }
-
-        int rate_;
-        int count_;
-        bool advertised_;
-        size_t latch_size_;
-        std::string output_name_;
-
-        ros::NodeHandle n_;
-        ros::Subscriber sub_;
-        ros::Publisher pub_;
-        ros::ServiceServer srv_rate_;
-
-        typedef topic_tools::ShapeShifter::ConstPtr MsgPtr;
-        typedef std::deque<MsgPtr> QueueType;
-        QueueType latch_buffer_;
-
-    };
+    ros::spin();
+    return 0;
 }
-
-PLUGINLIB_DECLARE_CLASS(topic_filters, GenericDivider,
-    topic_filters::GenericDivider, nodelet::Nodelet)
 

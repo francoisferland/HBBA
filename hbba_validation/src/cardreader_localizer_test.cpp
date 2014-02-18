f9b7f9b269ea5388c624809049d95b34d4294284
ferf2103 <ferf2103@23a761d4-b60a-4777-9503-f06a07968eac>
2014-02-18 15:12:19 +0000

    Refs #411. Fixing issues with cardreader localizer test.
    git-svn-id: svn+ssh://marvin.3it.usherbrooke.ca/svn/introlab/johnny0/svn/trunk/ros-pkgs/hbba@4104 23a761d4-b60a-4777-9503-f06a07968eac

--- hbba_validation/src/cardreader_localizer_test.cpp
+++ hbba_validation/src/cardreader_localizer_test.cpp
@@ -1,16 +1,17 @@
 #include <hbba_validation/cardreader_localizer.hpp>
 #include <ros/ros.h>
+#include <boost/scoped_ptr.hpp>
 
 namespace {
 
-    hbba_validation::CardreaderLocalizer localizer_;
+    boost::scoped_ptr<hbba_validation::CardreaderLocalizer> localizer_;
 
     void imageCB(const sensor_msgs::ImagePtr& img)
     {
         int x, y, c;
-        c = localizer_.process(img, x, y);
+        c = localizer_->process(img, x, y);
         ROS_INFO("Red   c: %i, x: %i, y: %i", c, x, y);
-        c = localizer_.process(img, x, y, true);
+        c = localizer_->process(img, x, y, true);
         ROS_INFO("Green c: %i, x: %i, y: %i", c, x, y);
     }
 
@@ -20,10 +21,11 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "cardreader_localizer");
 
     ros::NodeHandle n, np("~");
+    localizer_.reset(new hbba_validation::CardreaderLocalizer());
 
     int t;
     np.param("threshold", t, 32);
-    localizer_.threshold(t);
+    localizer_->threshold(t);
 
     ros::Subscriber sub_image_ = n.subscribe("image", 1, imageCB);
 

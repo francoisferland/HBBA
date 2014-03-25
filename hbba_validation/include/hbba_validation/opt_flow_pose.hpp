ae9b667dfbcb8b11f7860f9219870d57a4b9fbf4
ferf2103 <ferf2103@23a761d4-b60a-4777-9503-f06a07968eac>
2014-03-25 19:25:26 +0000

    Refs #411. Bounds on optical flow reaction, fix a crash when the IMV image is not properly detected as a panamorph lense, diagnostic output of optical flow sum, correct pose creation.
    git-svn-id: svn+ssh://marvin.3it.usherbrooke.ca/svn/introlab/johnny0/svn/trunk/ros-pkgs/hbba@4186 23a761d4-b60a-4777-9503-f06a07968eac

--- hbba_validation/include/hbba_validation/opt_flow_pose.hpp
+++ hbba_validation/include/hbba_validation/opt_flow_pose.hpp
@@ -20,6 +20,7 @@ namespace hbba_validation
     ///  - image_imv_raw: Raw IMV feed.
     ///  - motion_pose:   Pose of the most intense motion, in IMV's frame.
     ///  - ~proc_image:   Processed IMV image used as input.
+    ///  - ~opt_flow_sum: Sum of dense optical flow (m00 moment).
     /// 
     /// Parameters:
     ///  - imv_width:     Width of the IMV processed image.
@@ -30,6 +31,14 @@ namespace hbba_validation
     ///                   Default: 160.
     ///  - pose_dist:     Distance at which to generate the pose.
     ///                   Default: 1.0 m.
+    ///  - opt_flow_min:  Minimum flow sum (m00) to use to send a pose.
+    ///                   A low value might produce a behavior that is too
+    ///                   reactive.
+    ///                   Default: 3000.0.
+    ///  - opt_flow_max:  Maximum flow sum (m00) to use to send a pose.
+    ///                   An high value might produce a beahvior that reacts to
+    ///                   the robot's own movement.
+    ///                   Default: 50000.0.
     ///
     class OptFlowPose: private image_tools::DenseOpticalFlow
     {
@@ -37,11 +46,14 @@ namespace hbba_validation
         ros::Subscriber                sub_image_;
         ros::Publisher                 pub_pose_;
         ros::Publisher                 pub_proc_image_;
+        ros::Publisher                 pub_m00_;
         imv_camera::IMVCameraProcessor imv_proc_;
 
         int    imv_width_;
         int    imv_height_;
         double pose_dist_;
+        double opt_flow_min_;
+        double opt_flow_max_;
 
         std_msgs::Header last_header_;
 
--- hbba_validation/src/opt_flow_pose.cpp
+++ hbba_validation/src/opt_flow_pose.cpp
@@ -1,5 +1,6 @@
 #include <hbba_validation/opt_flow_pose.hpp>
 #include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/Float64.h>
 #include <opencv2/imgproc/imgproc.hpp>
 
 using namespace hbba_validation;
@@ -7,12 +8,16 @@ using namespace hbba_validation;
 OptFlowPose::OptFlowPose(ros::NodeHandle& n, ros::NodeHandle& np):
     image_tools::DenseOpticalFlow(np)
 {
-    np.param("pose_dist", pose_dist_, 1.0);
+    np.param("pose_dist",    pose_dist_,        3.0);
+    np.param("opt_flow_min", opt_flow_min_,  2500.0);
+    np.param("opt_flow_max", opt_flow_max_, 50000.0);
 
     sub_image_ = n.subscribe("image_imv_raw", 10, &OptFlowPose::imageCB, this);
 
     pub_pose_ = n.advertise<geometry_msgs::PoseStamped>("motion_pose", 10);
     pub_proc_image_ = np.advertise<sensor_msgs::Image>("proc_image", 10);
+
+    pub_m00_ = np.advertise<std_msgs::Float64>("opt_flow_sum", 10);
 
     imv_proc_.mode(imv_camera::MODE_PERI);
     imv_proc_.position(imv_camera::POSITION_WALL);
@@ -39,14 +44,24 @@ void OptFlowPose::flowCB(const cv::Mat& flow)
 {
     cv::Moments mu = cv::moments(flow);
 
-    // TODO: Change 1e-3 as a bounded threshold (min/max amount of motion):
-    if (mu.m00 < 1e-3) {
+    if (pub_m00_.getNumSubscribers() > 0) {
+        std_msgs::Float64 msg_m00;
+        msg_m00.data = mu.m00;
+        pub_m00_.publish(msg_m00);
+    }
+
+    if ((mu.m00 < opt_flow_min_) || (mu.m00 > opt_flow_max_)) {
         return;
     }
 
     cv::Point pt(mu.m10 / mu.m00, mu.m01 / mu.m00);
 
-    ROS_DEBUG_THROTTLE(1.0, "Current centroid: (%i, %i)", pt.x, pt.y);
+    ROS_DEBUG_THROTTLE(
+        1.0, 
+        "Current centroid, sum: (%i, %i), %f", 
+        pt.x, 
+        pt.y, 
+        mu.m00);
 
     double pan, tilt;
     imv_proc_.orientationFromOutputPosition(pt.x, pt.y, pan, tilt);
@@ -54,10 +69,8 @@ void OptFlowPose::flowCB(const cv::Mat& flow)
     geometry_msgs::PoseStamped pose;
     pose.header = last_header_;
 
-    // TODO: Make sure we're generating the pose in the correct orientation (X
-    // in front, Y to the left ?)
-    pose.pose.position.x    = pose_dist_ * cos(tilt) * cos(pan);
-    pose.pose.position.y    = pose_dist_ * cos(tilt) * sin(pan);
+    pose.pose.position.x    = pose_dist_ * cos(tilt) * cos(-pan);
+    pose.pose.position.y    = pose_dist_ * cos(tilt) * sin(-pan);
     pose.pose.position.z    = pose_dist_ * sin(tilt);
     pose.pose.orientation.x = 0.0;
     pose.pose.orientation.y = 0.0;

e8be2aa08cc59a33ec914470dc85f5d572a08717
François Ferland <git@francoisferland.com>
2014-04-04 13:25:49 +0200

    Merge update.

--- or_tools/install.sh
+++ or_tools/install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-cp $PWD/lib/*.so $1/
+cp $PWD/lib/*.so $1/lib/
+
+mkdir -p $1/include/or-tools/constraint_solver
+cp $PWD/src/constraint_solver/*.h $1/include/or-tools/constraint_solver
+mkdir -p $1/include/or-tools/base
+cp $PWD/src/base/*.h $1/include/or-tools/base
+mkdir -p $1/include/or-tools/util
+cp $PWD/src/util/*.h $1/include/or-tools/util
 

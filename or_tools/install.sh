c098aed47ba093b0d85b9c52b95eb706d0579f42
François Ferland <git@francoisferland.com>
2014-05-09 15:21:50 +0200

    Fixes or_tools output path

--- or_tools/install.sh
+++ or_tools/install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-mkdir -p $1/lib/
-cp $PWD/lib/*.so $1/lib/
+INCPATH=$1/include/or-tools
+LIBPATH=$1/lib/or-tools
 
-mkdir -p $1/include/or-tools/constraint_solver
-cp $PWD/src/constraint_solver/*.h $1/include/or-tools/constraint_solver
-mkdir -p $1/include/or-tools/base
-cp $PWD/src/base/*.h $1/include/or-tools/base
-mkdir -p $1/include/or-tools/util
-cp $PWD/src/util/*.h $1/include/or-tools/util
+mkdir -p $LIBPATH
+cp $PWD/lib/*.so $LIBPATH
+
+mkdir -p $INCPATH/constraint_solver
+cp $PWD/src/constraint_solver/*.h $INCPATH/constraint_solver
+mkdir -p $INCPATH/base
+cp $PWD/src/base/*.h $INCPATH/base
+mkdir -p $INCPATH/util
+cp $PWD/src/util/*.h $INCPATH/util
 

8f533b04ad5dd43bb545f3668e27a2bd1c2088a5
François Ferland <git@francoisferland.com>
2015-11-12 15:40:09 +0100

    Removing packages now in hbba_base.

--- iw/setup.py
+++ iw/setup.py
@@ -5,10 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
         ##  don't do this unless you want a globally visible script
-        scripts=['scripts/iw',
-                 'scripts/iw_console',
-                 'scripts/iw_observer',
-                 'scripts/get_intention'], 
+        scripts=['scripts/iw'],
         packages=['iw'],
         package_dir={'': 'src'}
         )

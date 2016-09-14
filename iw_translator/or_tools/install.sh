1b9240d8f59bee0f2a0adf3022ce3a59a33af615
François Ferland <git@francoisferland.com>
2016-09-14 18:15:29 +0200

    Switching to master branch of or-tools to resolve broken sub-dependencies.

--- iw_translator/or_tools/install.sh
+++ iw_translator/or_tools/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+GENPATH=$2
 INCPATH=$1/include/or-tools
 LIBPATH=$1/lib/or-tools
 
@@ -8,6 +9,7 @@ cp $PWD/lib/*.so $LIBPATH
 
 mkdir -p $INCPATH/constraint_solver
 cp $PWD/src/constraint_solver/*.h $INCPATH/constraint_solver
+cp $PWD/src/gen/constraint_solver/*.h $INCPATH/constraint_solver 
 mkdir -p $INCPATH/base
 cp $PWD/src/base/*.h $INCPATH/base
 mkdir -p $INCPATH/util

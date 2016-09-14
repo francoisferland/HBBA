ca53e9397020e6ceb41a83fc0b4888053bdc814e
François Ferland <git@francoisferland.com>
2016-09-14 15:06:11 +0200

    Adding a new script engine plugin for Float64 publication.

---
+++ se_extra_plugins/src/se_extra_plugins.cpp
@@ -0,0 +1,36 @@
+#include <script_engine/publisher_topic_arg_base.hpp>
+#include <std_msgs/Float64.h>
+#include <pluginlib/class_list_macros.h>
+#include <v8.h>
+
+namespace se_extra_plugins
+{
+    extern void pubFloat64Fun(const v8::Arguments& args,
+                              std_msgs::Float64&   msg)
+    {
+        if (args.Length() < 2) {
+            ROS_ERROR("Not enough arguments to pubFloat64(topic, value).");
+            return;
+        }
+
+        v8::Number* v = v8::Number::Cast(*args[1]);
+
+        ROS_DEBUG("Sending float value %f", v->Value());
+
+        msg.data = v->Value();
+
+        return;
+    }
+
+    const extern char pub_float64_name[] = "pubFloat64";
+    typedef script_engine::publisher_topic_arg_base<
+        std_msgs::Float64,
+        pub_float64_name,
+        pubFloat64Fun> PubFloat64Plugin;
+
+}
+
+PLUGINLIB_DECLARE_CLASS(se_extra_plugins,
+                        PubFloat64Plugin,
+                        se_extra_plugins::PubFloat64Plugin,
+                        script_engine::engine_module);

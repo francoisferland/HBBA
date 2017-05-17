01578f886e500856d7e4c9380d954221784bf510
François Ferland <git@francoisferland.com>
2017-05-17 14:47:48 +0000

    Fixing a permission issue in the generate Debian package for or-tools.

--- iw_translator/or_tools/generate_dpkg.sh
+++ iw_translator/or_tools/generate_dpkg.sh
@@ -8,11 +8,20 @@ PACKAGE=or-tools_Ubuntu-14.04-64bit_v5.1.4045
 FOLDER=or-tools_Ubuntu-14.04-64bit_v5.1.4047    # NOTE: The content doesn't fit
 URL=https://github.com/google/or-tools/releases/download/v5.1/${PACKAGE}.tar.gz 
 
-wget $URL
-tar -xzvf $PACKAGE.tar.gz
+if [ ! -f $PACKAGE.tar.gz ]; then
+    wget $URL
+fi
+if [ ! -f $FOLDER ]; then
+    tar -xzvf $PACKAGE.tar.gz
+fi
+
 mkdir -p dpkg/opt/or-tools/
 cp -r $FOLDER/include dpkg/opt/or-tools/
 cp -r $FOLDER/lib dpkg/opt/or-tools/
 rm dpkg/opt/or-tools/lib/*.jar
+
+find dpkg/opt -type d -exec chmod 755 {} +
+find dpkg/opt -name *.so -exec chmod 755 {} +
+find dpkg/opt -name *.h -exec chmod 644 {} +
 
 dpkg-deb --build dpkg or-tools_5.1.4047-1.deb

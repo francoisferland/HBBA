7f74c7cfca8b58230225ff886fe6b15bdadcd28b
François Ferland <git@francoisferland.com>
2019-06-19 12:15:58 -0400

    Updated script to install or-tools to the latest version.

--- iw_translator/or_tools/generate_dpkg.sh
+++ iw_translator/or_tools/generate_dpkg.sh
@@ -4,9 +4,13 @@
 # or-tools in the current folder (it assumes that a 'dpkg' folder exists and
 # contains the package config.
 
-PACKAGE=or-tools_Ubuntu-14.04-64bit_v5.1.4045
-FOLDER=or-tools_Ubuntu-14.04-64bit_v5.1.4047    # NOTE: The content doesn't fit
-URL=https://github.com/google/or-tools/releases/download/v5.1/${PACKAGE}.tar.gz 
+# PACKAGE=or-tools_Ubuntu-14.04-64bit_v5.1.4045
+# FOLDER=or-tools_Ubuntu-14.04-64bit_v5.1.4047    # NOTE: The content doesn't fit
+# URL=https://github.com/google/or-tools/releases/download/v5.1/${PACKAGE}.tar.gz 
+
+PACKAGE=or-tools_ubuntu-18.04_v7.1.6720
+FOLDER=or-tools_Ubuntu-18.04-64bit_v7.1.6720
+URL=https://github.com/google/or-tools/releases/download/v7.1/or-tools_ubuntu-18.04_v7.1.6720.tar.gz
 
 if [ ! -f $PACKAGE.tar.gz ]; then
     wget $URL
@@ -24,4 +28,4 @@ find dpkg/opt -type d -exec chmod 755 {} +
 find dpkg/opt -name *.so -exec chmod 755 {} +
 find dpkg/opt -name *.h -exec chmod 644 {} +
 
-dpkg-deb --build dpkg or-tools_5.1.4047-1.deb
+dpkg-deb --build dpkg $PACKAGE.deb
